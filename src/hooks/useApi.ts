8796aa25e111bcdf7d13f283a36aff6ccf07734f
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-06 21:15:15 -0300

    created axios config and custom hook

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import { api } from '../services/api';
+
+export const useApi = () => {
+
+    const fetchData = async(url: string) => {
+        try{
+            const response = await api.get(url);
+            return response.data;
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    return fetchData;
+}
