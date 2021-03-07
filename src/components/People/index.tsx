7f8f62e00eb0619343ac55ef98f79e446044404e
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-06 21:14:51 -0300

    initialized people component

---
+++ src/components/People/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const People: React.FC = () => {
+  return (
+    <Container>
+      <h1>People</h1>
+    </Container>
+  );
+};
+
+export default People;
