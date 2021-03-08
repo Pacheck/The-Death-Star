3d6bab9c353ae32b8c57a8b4eafd72dbe9e0c7c2
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-08 02:53:15 -0300

    added routes and removed react import

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,15 +1,28 @@
-import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Films from "../components/Films";
 import Home from "../components/Home";
-function App() {
+import People from "../components/People";
+import Planets from "../components/Planets";
+import Species from "../components/Species";
+import Starships from "../components/Starships";
+import Vehicles from "../components/Vehicles";
+
+const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/films" component={Films} />
+        <Route path="/people" component={People} />
+        <Route path="/planets" component={Planets} />
+        <Route path="/species" component={Species} />
+        <Route path="/starships" component={Starships} />
+        <Route path="/vehicles" component={Vehicles} />
       </Switch>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
 // films, people, planets, species, starships, vehicles
--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,4 +1,3 @@
-import React from 'react';
 import { api } from '../services/api';
 
 export const useApi = () => {
