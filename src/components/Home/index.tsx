401448c1cf45317158791c4ceb1db363a0e40d34
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-08 02:51:21 -0300

    created home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,59 @@
-import React from "react";
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
 import { Container } from "./styles";
 
 const Home = () => {
+  const history = useHistory();
+
+  const handleNavigate = (path: string) => {
+    history.push(path);
+  };
+
   return (
     <Container>
-      <h1>Home</h1>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={() => handleNavigate("/films")}
+      >
+        Films
+      </Button>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={() => handleNavigate("/people")}
+      >
+        People
+      </Button>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={() => handleNavigate("/planets")}
+      >
+        Planets
+      </Button>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={() => handleNavigate("/species")}
+      >
+        Species
+      </Button>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={() => handleNavigate("/starships")}
+      >
+        Starships
+      </Button>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={() => handleNavigate("/vehicles")}
+      >
+        Vehicles
+      </Button>
     </Container>
   );
 };
