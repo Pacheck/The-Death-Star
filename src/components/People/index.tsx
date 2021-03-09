b536b4beba51e8db87350525916e798d46c994aa
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-08 23:50:37 -0300

    added flex, removed items

--- src/components/People/index.tsx
+++ src/components/People/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useApi } from "../../hooks/useApi";
+import PeopleCard from "../PeopleCard";
 import { Container } from "./styles";
 import { Person } from "./types";
 
@@ -10,6 +11,7 @@ const People = () => {
   const fetchPeopleData = async () => {
     const response = await fetchData("http://swapi.dev/api/people/");
     setPeople(response.results);
+    console.log(response.results);
   };
 
   useEffect(() => {
@@ -18,11 +20,14 @@ const People = () => {
 
   return (
     <Container>
-      <ul>
-        {people.map((person) => (
-          <li key={person.name}>{person.name}</li>
-        ))}
-      </ul>
+      {people.map((person) => (
+        <PeopleCard
+          name={person.name}
+          mass={person.mass}
+          height={person.height}
+          birth_year={person.birth_year}
+        />
+      ))}
     </Container>
   );
 };
