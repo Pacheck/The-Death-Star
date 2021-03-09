bd17fb6b19aa050a59ebda9fd1991a3de378481b
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-08 23:51:09 -0300

    created globalStyle

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -8,21 +8,25 @@ import Species from "../components/Species";
 import Starships from "../components/Starships";
 import Vehicles from "../components/Vehicles";
 
+import GlobalStyle from "./globalStyles";
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/films" component={Films} />
-        <Route path="/people" component={People} />
-        <Route path="/planets" component={Planets} />
-        <Route path="/species" component={Species} />
-        <Route path="/starships" component={Starships} />
-        <Route path="/vehicles" component={Vehicles} />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/films" component={Films} />
+          <Route path="/people" component={People} />
+          <Route path="/planets" component={Planets} />
+          <Route path="/species" component={Species} />
+          <Route path="/starships" component={Starships} />
+          <Route path="/vehicles" component={Vehicles} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 };
 
 export default App;
-// films, people, planets, species, starships, vehicles
---
+++ src/container/globalStyles.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        text-decoration: none;
+        list-style: none;
+    }
+`
+
+export default GlobalStyle;
