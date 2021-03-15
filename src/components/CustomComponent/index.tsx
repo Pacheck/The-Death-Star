e8855282ea07ff69207b78d5601f95587db56e6c
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-15 13:57:30 -0300

    updated

--- src/components/CustomComponent/index.tsx
+++ src/components/CustomComponent/index.tsx
@@ -36,7 +36,7 @@ const CustomComponent = () => {
   const handleFetchDataFromAPI = async () => {
     setIsLoading(true);
     const response: Response = await fetchData(
-      `${pathname}?page=${currentPage}`
+      `${pathname}?page=${currentPage}/`
     );
     console.log(response);
     setData(response.results);
