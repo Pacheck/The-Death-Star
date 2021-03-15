b6a4062d726ff64be9474c43eff6d3ce33f12eb1
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-14 23:52:11 -0300

    created custom card with loading condition

--- src/components/CustomCard/index.tsx
+++ src/components/CustomCard/index.tsx
@@ -1,10 +1,14 @@
+import { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-import { CardData } from "./types";
+
+import Skeleton from "@material-ui/lab/Skeleton";
+
+import { PropData, Dados } from "./types";
 
 const useStyles = makeStyles({
   root: {
@@ -17,35 +21,121 @@ const useStyles = makeStyles({
   media: {
     height: 140,
   },
+  text: {
+    marginBottom: 6,
+  },
 });
 
-const DinamicCard = ({ text1, text2, text3, text4, imageLogo }: CardData) => {
+const DinamicCard = ({ data, imageLogo, isLoading }: PropData) => {
+  const [dados, setDados] = useState<Dados>();
   const classes = useStyles();
 
+  const handleViewData = () => {
+    setDados({
+      text1: data.name || data.title || "unknown",
+      text2:
+        data.director ||
+        data.mass ||
+        data.population ||
+        data.classification ||
+        data.crew ||
+        data.model ||
+        "unknown",
+      text3:
+        data.release_date ||
+        data.height ||
+        data.climate ||
+        data.language ||
+        data.max_atmosphering_speed ||
+        data.vehicle_class ||
+        "unknown",
+      text4:
+        data.opening_crawl ||
+        data.birth_year ||
+        data.terrain ||
+        data.skin_colors ||
+        data.cost_in_credits ||
+        data.cost_in_credits ||
+        "unknown",
+      imageLogo: imageLogo,
+    });
+  };
+
+  useEffect(() => {
+    handleViewData();
+  }, []);
+
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={imageLogo}
-          title="StarWars Logo"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {text1}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="h4">
-            {text2}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="h6">
-            {text3}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {text4}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <>
+      {!!dados && (
+        <Card className={classes.root}>
+          <CardActionArea>
+            {isLoading ? (
+              <Skeleton
+                animation="wave"
+                variant="rect"
+                className={classes.media}
+              />
+            ) : (
+              <CardMedia
+                className={classes.media}
+                image={dados.imageLogo}
+                title="StarWars Logo"
+              />
+            )}
+            <CardContent>
+              {isLoading ? (
+                <>
+                  <Skeleton
+                    animation="wave"
+                    height={10}
+                    className={classes.text}
+                    width="50%"
+                  />
+                  <Skeleton
+                    animation="wave"
+                    height={10}
+                    width="70%"
+                    className={classes.text}
+                  />
+                  <Skeleton
+                    animation="wave"
+                    height={10}
+                    width="40%"
+                    className={classes.text}
+                  />
+                  <Skeleton
+                    animation="wave"
+                    height={10}
+                    width="80%"
+                    className={classes.text}
+                  />
+                </>
+              ) : (
+                <>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {dados.text1}
+                  </Typography>
+                  <Typography gutterBottom variant="h6" component="h4">
+                    {dados.text2}
+                  </Typography>
+                  <Typography gutterBottom variant="h6" component="h6">
+                    {dados.text3}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {dados.text4}
+                  </Typography>
+                </>
+              )}
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      )}
+    </>
   );
 };
 
--- src/components/CustomCard/types.ts
+++ src/components/CustomCard/types.ts
@@ -1,9 +1,15 @@
-import { CardMediaTypeMap } from "@material-ui/core";
+import { Film, Vehicle, Person, Planet, Specie, Starship } from "../CustomComponent/types";
 
-export interface CardData {
+export interface PropData {
+    data: Film | Vehicle | Person | Planet | Specie | Starship | any;
+    imageLogo: string;
+    isLoading: boolean;
+}
+
+export interface Dados {
     text1: string;
     text2: string;
     text3: string;
     text4: string;
-    imageLogo:  string;
+    imageLogo: string;
 }
