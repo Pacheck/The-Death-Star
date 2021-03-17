448f5e4481c02b4a65798d2fd8a5fe57bf1864e6
Pedro Pacheco <pacheckdev@gmail.com>
2021-03-17 18:07:54 -0300

    created custom type with generic and keys

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,22 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { APIResponse } from "../../components/CustomComponent/types";
 import { useApi } from "../../hooks/useApi";
+import { v4 as uuidv4 } from "uuid";
+
+import { Container } from "./styles";
+import { DynamicObject } from "./types";
 
 const Details = () => {
+  const [data, setData] = useState<DynamicObject<APIResponse>>({});
   const { pathname } = useLocation();
   const fetchData = useApi();
 
   const handleFetchDataFromAPI = async () => {
     const urlToFetch = pathname.replace("/details/", "");
-    const response = await fetchData(urlToFetch);
-    console.log(response);
+    const response: DynamicObject<APIResponse> = await fetchData(urlToFetch);
+    setData(response);
   };
 
   useEffect(() => {
     handleFetchDataFromAPI();
   }, []);
 
-  return <h2>details</h2>;
+  return (
+    <>
+      {!!data && (
+        <Container>
+          {Object.keys(data).map((key) => {
+            return (
+              <h3 key={uuidv4()}>
+                {key}: {data[key]}
+              </h3>
+            );
+          })}
+        </Container>
+      )}
+    </>
+  );
 };
 
 export default Details;
